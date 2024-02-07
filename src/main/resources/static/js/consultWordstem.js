a50b5cc110107fd1b9ed01d69bcc536c3237a824
Kerbetth <kerbetth@protonmail.com>
2024-02-07 07:20:38 +0100

    replace thymeleaf with a javascript structure on wordstems-show.html (work in progress)

--- src/main/resources/static/js/consultWordstem.js
+++ src/main/resources/static/js/consultWordstem.js
@@ -5,12 +5,15 @@ let pageCount = 0;
 let wordstemList = 0;
 let data;
 
+
+const countElement = document.getElementById("wordstems-count")
+const wordStemNameElement = document.getElementById("wordstems-name")
 const currentNameElement = document.getElementById("currentName")
 
 
 
 
-async function getWordstemData() {
+async function getShowWordstem() {
     try {
         const response = await fetch(host +apiWordstem+"?pageSize="+countByPage,{
             method: "GET",
@@ -29,7 +32,19 @@ async function getWordstemData() {
         console.error(console.log(error));
     }
 
-    getWordstemData();
+    function showWordstems(){
+        getShowWordstem()
+        for(i=0;i<data.pageWordstems.length;i++){
+        let wsName = document.createElement("name");
+        let wsPhonetic = document.createElement("phonetic");
+        let wsLang = document.createElement("lang");
+        let wsGender = document.createElement("gender");
+        let wsRefFr = document.createElement("fr");
+        let wsParent = document.createElement("parent");
+        let wsDel = document.createElement("parent");
+        wsName.innerText(data.pageWordstems[i].wordStemName);
+        }
+    };
 
 
 
