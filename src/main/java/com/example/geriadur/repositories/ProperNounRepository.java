68815709d7a90f21185539a10adf9cf6940e8f7b
Kerbetth <kerbetth@protonmail.com>
2024-04-05 12:20:47 +0200

    change repository name from EtymonName to ProperNoun in order to be more undestandable

---
+++ src/main/java/com/example/geriadur/repositories/ProperNounRepository.java
@@ -0,0 +1,20 @@
+package com.example.geriadur.repositories;
+
+import com.example.geriadur.entity.ProperNoun;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Repository
+public interface ProperNounRepository extends JpaRepository<ProperNoun, Long> {
+    Optional<ProperNoun> findProperNounByCurrentName(String currentName);
+    @Query(value = "SELECT * FROM propernoun where word_theme = ?1 ORDER BY RAND() LIMIT 15",
+            nativeQuery = true)
+    Set<ProperNoun> find15ProperNounsByWordTheme(@Param("word_theme")int wordTheme);
+
+    Set<ProperNoun> findAllProperNounsByWordTheme(@Param("word_theme")int wordTheme);
+}
